0956d8f41b768cd47ff769efeb43a153c5650107
Tamas Szucs <tomfurrier@gmail.com>
2014-08-12 10:48:06 +0200

    formatted output

--- random_quote.rb
+++ random_quote.rb
@@ -2,6 +2,25 @@ require 'open-uri'
 require 'json'
 load 'bash-string-escaper/bash_string_escape.rb'
 
+$formatted_output_file_path = ENV['BITRISE_STEP_FORMATTED_OUTPUT_FILE_PATH']
+
+def puts_string_to_formatted_output(text)
+  open($formatted_output_file_path, 'a') { |f|
+    f.puts(text)
+  }
+end
+
+def puts_section_to_formatted_output(section_text)
+  open($formatted_output_file_path, 'a') { |f|
+    f.puts
+    f.puts(section_text)
+    f.puts
+  }
+end
+
+puts_section_to_formatted_output('# Random Quote')
+
+
 uri_string = "http://www.iheartquotes.com/api/v1/random?format=json&source=humorix_misc"
 uri = URI.parse(uri_string)
 begin
@@ -25,8 +44,15 @@ begin
   puts "Escaped:"
   puts escaped_quote_text
   File.open(File.join(ENV['HOME'], '.bash_profile'), 'a') { |f| f.write("export RANDOM_QUOTE=\"#{escaped_quote_text}\"\n") }
+  puts_section_to_formatted_output("\"#{escaped_quote_text}\"")
 rescue => ex
   puts "Request error: #{ex}"
+  err_msg = "Exception happened: #{ex}"
+  puts err_msg
   puts " [i] uri_string: #{uri_string}"
-  exit 1
+  puts_section_to_formatted_output("## Failed")
+  unless err_msg.nil?
+    puts_section_to_formatted_output(err_msg)
+  end
+  puts_section_to_formatted_output("Check the Logs for details.")
 end
